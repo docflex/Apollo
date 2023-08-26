7ff7bc757326c196f2b83061b643523f4b74e11f
Rehber Moin <rehbermoin.io@gmail.com>
2023-08-26 22:15:06 +0530

    [ADDED]: Routes for Dashboard and Redirects to Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
+import { Dashboard } from "@mui/icons-material";
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { createMuiTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
-
 
 function App() {
     const mode = useSelector((state) => state.global.mode);
     const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
     return (
         <div className="app">
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-            </ThemeProvider>
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Routes>
+                        <Route element={<Layout />}>
+                            <Route
+                                path="/"
+                                element={<Navigate to="/dashboard" replace />}
+                            />
+                            <Route path="/dashboard" element={<Dashboard />} />
+                        </Route>
+                    </Routes>
+                </ThemeProvider>
+            </BrowserRouter>
         </div>
     );
 }

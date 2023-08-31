11b9c1e36009f54ffcc223e16a99bd59c920e403
Rehber Moin <rehbermoin.io@gmail.com>
2023-09-01 00:53:35 +0530

    [ADDED]: Transaction Page + Controller + Route

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Layout from "scenes/layout";
 import Dashboard from "scenes/dashboard";
 import Products from "scenes/products";
 import Customers from "scenes/customers";
+import Transactions from "scenes/transactions";
 
 function App() {
     const mode = useSelector((state) => state.global.mode);
@@ -23,6 +24,7 @@ function App() {
                             <Route path="/dashboard" element={<Dashboard />} />
                             <Route path="/products" element={<Products />} />
                             <Route path="/customers" element={<Customers />} />
+                            <Route path="/transactions" element={<Transactions />} />
                         </Route>
                     </Routes>
                 </ThemeProvider>
--- server/controllers/client.js
+++ server/controllers/client.js
@@ -3,6 +3,8 @@ import ProductStat from "../models/ProductStat.js";
 
 import User from "../models/User.js";
 
+import Transaction from "../models/Transaction.js";
+
 export const getProducts = async (req, res) => {
     try {
         const products = await Product.find();
@@ -28,6 +30,53 @@ export const getCustomers = async (req, res) => {
     try {
         const customers = await User.find({ role: "user" }).select("-password");
         res.status(200).json(customers);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
+
+/**
+ * For this Endpoint, we will be doing Server Side Pagination.
+ * 1. Grab the values from our Query String.  (For Convenience, we Assume Page = 1, PageSize = 20, sorted = Null and SearchQuery = "")
+ */
+
+export const getTransactions = async (req, res) => {
+    try {
+        // sort should look like this: { "field": "userId", "sort": "desc"}
+        const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+
+        // formatted sort should look like { userId: -1 }
+        const generateSort = () => {
+            const sortParsed = JSON.parse(sort);
+            const sortFormatted = {
+                [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+            };
+
+            return sortFormatted;
+        };
+        const sortFormatted = Boolean(sort) ? generateSort() : {};
+
+        const transactions = await Transaction.find({
+            $or: [
+                { cost: { $regex: new RegExp(search, "i") } },
+                { userId: { $regex: new RegExp(search, "i") } },
+            ],
+        })
+            .sort(sortFormatted)
+            .skip(page * pageSize)
+            .limit(pageSize);
+
+        const total = await Transaction.countDocuments({
+            $or: [
+                { cost: { $regex: new RegExp(search, "i") } },
+                { userId: { $regex: new RegExp(search, "i") } },
+            ],
+        });
+
+        res.status(200).json({
+            transactions,
+            total,
+        });
     } catch (error) {
         res.status(404).json({ message: error.message });
     }

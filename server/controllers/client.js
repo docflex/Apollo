4942eb174dc862952659399cd5466e9785999892
Rehber Moin <rehbermoin.io@gmail.com>
2023-09-01 01:18:40 +0530

    [ADDED]: Geography Page + Routes + Controllers

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,3 +1,5 @@
+import getCountryIso3 from "country-iso-2-to-3";
+
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 
@@ -77,6 +79,27 @@ export const getTransactions = async (req, res) => {
             transactions,
             total,
         });
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
+
+export const getGeography = async (req, res) => {
+    try {
+        const users = await User.find();
+        const mappedLocations = users.reduce((acc, { country }) => {
+            const countryISO3 = getCountryIso3(country);
+            if (!acc[countryISO3]) {
+                acc[countryISO3] = 0;
+            }
+            acc[countryISO3]++;
+            return acc;
+        }, {});
+
+        const formattedLocation = Object.entries(mappedLocations).map(([country, count]) => {
+            return { id: country, value: count };
+        });
+        res.status(200).json(formattedLocation);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }

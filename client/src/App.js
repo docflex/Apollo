4c7700d27923cdd1dde57cbf096252b950fc0a0e
Rehber Moin <rehbermoin.io@gmail.com>
2023-09-01 22:11:32 +0530

    [ADDED]: Breakdown Page + Route

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Geography from "scenes/geography";
 import Overview from "scenes/overview";
 import Daily from "scenes/daily";
 import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
 
 function App() {
     const mode = useSelector((state) => state.global.mode);
@@ -33,6 +34,7 @@ function App() {
                             <Route path="/overview" element={<Overview />} />
                             <Route path="/daily" element={<Daily />} />
                             <Route path="/monthly" element={<Monthly />} />
+                            <Route path="/breakdown" element={<Breakdown />} />
                         </Route>
                     </Routes>
                 </ThemeProvider>

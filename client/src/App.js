a60a7eabce01214c1c0defeb01a4351e29d66da9
Rehber Moin <rehbermoin.io@gmail.com>
2023-09-01 22:06:43 +0530

    [ADDED]: Monthly Page + Routes

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
 import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
 
 function App() {
     const mode = useSelector((state) => state.global.mode);
@@ -31,6 +32,7 @@ function App() {
                             <Route path="/geography" element={<Geography />} />
                             <Route path="/overview" element={<Overview />} />
                             <Route path="/daily" element={<Daily />} />
+                            <Route path="/monthly" element={<Monthly />} />
                         </Route>
                     </Routes>
                 </ThemeProvider>

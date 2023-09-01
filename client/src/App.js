ec1d705ea8fffb45cd06ff716d0443035d3bb3f9
Rehber Moin <rehbermoin.io@gmail.com>
2023-09-01 22:02:40 +0530

    [ADDED]: Daily Page + Routes

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
+import Daily from "scenes/daily";
 
 function App() {
     const mode = useSelector((state) => state.global.mode);
@@ -29,6 +30,7 @@ function App() {
                             <Route path="/transactions" element={<Transactions />} />
                             <Route path="/geography" element={<Geography />} />
                             <Route path="/overview" element={<Overview />} />
+                            <Route path="/daily" element={<Daily />} />
                         </Route>
                     </Routes>
                 </ThemeProvider>

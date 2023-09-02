05ae504400898a27970266ba8b842ec5e9c5c249
Rehber Moin <rehbermoin.io@gmail.com>
2023-09-02 23:09:01 +0530

    [OVERRIDE]: Production Name Change + CSS Fix + One-time Patch

--- client/src/components/SidebarComponents/SidebarHeader.jsx
+++ client/src/components/SidebarComponents/SidebarHeader.jsx
@@ -2,20 +2,29 @@ import React from "react";
 import { Typography, IconButton, Box } from "@mui/material";
 import { ChevronLeft } from "@mui/icons-material";
 import FlexBetween from "components/Utils/FlexBetween";
+import Logo from "assets/Logo.png";
 
 function SidebarHeader({ isNonMobile, isSidebarOpen, setIsSidebarOpen }) {
     return (
         <Box m="1.5rem 2rem 2rem 3rem">
             <FlexBetween>
-                <Box display="flex" alignItems="center" gap="0.5rem">
-                    <Typography variant="h4" fontWeight="bold">
-                        ECOMVISION
-                    </Typography>
+                <Box display="flex" alignItems="center">
+                    <Box
+                        marginLeft="-5rem"
+                        component="img"
+                        alt="logo"
+                        src={Logo}
+                        height="5rem"
+                        width="20rem"
+                        sx={{ objectFit: "contain" }}
+                    />
                 </Box>
                 {!isNonMobile && (
-                    <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-                        <ChevronLeft />
-                    </IconButton>
+                    <Box marginLeft="-5rem">
+                        <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                            <ChevronLeft />
+                        </IconButton>
+                    </Box>
                 )}
             </FlexBetween>
         </Box>
